facbbacfc0da5c206c00c3c0f21f3f81c225a544
Joel Adeniji <57308580+JOOOEG123@users.noreply.github.com>
2022-06-02 21:23:01 -0400

    home page template

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.scss']
+  styleUrls: ['./homepage.component.scss'],
 })
 export class HomepageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  fakeProfile = [
+    {
+      name: 'John Doe',
+      email: 'johnDoe@aol.com',
+    },
+    {
+      name: 'Jane Doe',
+      email: 'JaneDoe@aol.com',
+    },
+    {
+      name: 'John Smith',
+      email: 'johnSmith@aol.com',
+    },
+  ];
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
