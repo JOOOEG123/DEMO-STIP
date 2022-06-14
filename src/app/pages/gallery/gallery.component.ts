a4d12da0d38171509a587d4c1a93e37c3b2749e2
dangan114 <an.dang114@gmail.com>
2022-06-14 09:50:30 -0400

    Add masonry columns and click event for categories

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FilterTypes } from 'src/app/core/types/filters.type';
+import { NgxMasonryOptions } from 'ngx-masonry';
 
 @Component({
   selector: 'app-gallery',
@@ -7,10 +7,37 @@ import { FilterTypes } from 'src/app/core/types/filters.type';
   styleUrls: ['./gallery.component.scss']
 })
 export class GalleryComponent implements OnInit {
-  filterValues: FilterTypes = {} as FilterTypes;
+  selected: string | undefined
+  galleries: Array<string> = [
+    'All', 'Artifacts', 'Camps', 'People', 'Album Name'
+  ]
+
+  public masonryOptions: NgxMasonryOptions = {
+    gutter: 20
+  };
+
+  images: Array<string> = [
+    "https://i.imgur.com/C2FSuws.jpg",
+    "https://imgur.com/ugRuIHq.jpg",
+    "https://i.imgur.com/7tWYNvv.jpg",
+    "https://i.imgur.com/SeLoG8d.jpg",
+    "https://i.imgur.com/Xaha6Vo.jpg",
+    "https://i.imgur.com/bN4U2J0.jpg",
+    "https://i.imgur.com/3S1OoAi.jpg",
+    "https://i.imgur.com/3GHYEo3.jpg",
+    "https://i.imgur.com/ywAiaWh.jpg",
+    "https://i.imgur.com/wOPcRBD.jpg",
+    "https://i.imgur.com/Z9dsNRc.jpg",
+    "https://i.imgur.com/3HXgWpU.jpg"
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
+    this.selected = 'All'
   }
 
+  setActive(gallery: string) {
+    this.selected = gallery
+  }
 }
