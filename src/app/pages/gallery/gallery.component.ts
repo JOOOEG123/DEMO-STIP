b03b2f9ea68a96823227fb316dd841e1cdc26b6a
dangan114 <an.dang114@gmail.com>
2022-06-14 12:15:37 -0400

    Click event on individual image

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { NgxMasonryOptions } from 'ngx-masonry';
+
+interface Image {
+  src: string,
+  title: string,
+  description: string
+}
 
 @Component({
   selector: 'app-gallery',
@@ -7,7 +14,9 @@ import { NgxMasonryOptions } from 'ngx-masonry';
   styleUrls: ['./gallery.component.scss']
 })
 export class GalleryComponent implements OnInit {
-  selected: string | undefined
+  selectedCategory?: string
+  selectedImage?: Image
+
   galleries: Array<string> = [
     'All', 'Artifacts', 'Camps', 'People', 'Album Name'
   ]
@@ -16,28 +25,88 @@ export class GalleryComponent implements OnInit {
     gutter: 20
   };
 
-  images: Array<string> = [
-    "https://i.imgur.com/C2FSuws.jpg",
-    "https://imgur.com/ugRuIHq.jpg",
-    "https://i.imgur.com/7tWYNvv.jpg",
-    "https://i.imgur.com/SeLoG8d.jpg",
-    "https://i.imgur.com/Xaha6Vo.jpg",
-    "https://i.imgur.com/bN4U2J0.jpg",
-    "https://i.imgur.com/3S1OoAi.jpg",
-    "https://i.imgur.com/3GHYEo3.jpg",
-    "https://i.imgur.com/ywAiaWh.jpg",
-    "https://i.imgur.com/wOPcRBD.jpg",
-    "https://i.imgur.com/Z9dsNRc.jpg",
-    "https://i.imgur.com/3HXgWpU.jpg"
+  modelRef: BsModalRef | undefined
+
+  images: Array<Image> = [
+    {
+      src: "https://i.imgur.com/C2FSuws.jpg",
+      title: "Some title",
+      description: 'Some description'
+    },
+    {
+      src: "https://imgur.com/ugRuIHq.jpg",
+      title: "Some title",
+      description: 'Some description'
+    },
+    {
+      src: "https://imgur.com/ugRuIHq.jpg",
+      title: "Some title",
+      description: 'Some description'
+    },
+    {
+      src: "https://i.imgur.com/7tWYNvv.jpg",
+      title: "Some title",
+      description: 'Some description'
+    },
+    {
+      src: "https://i.imgur.com/SeLoG8d.jpg",
+      title: "Some title",
+      description: 'Some description'
+    },
+    {
+      src: "https://i.imgur.com/Xaha6Vo.jpg",
+      title: "Some title",
+      description: 'Some description'
+    },
+    {
+      src: "https://i.imgur.com/bN4U2J0.jpg",
+      title: "Some title",
+      description: 'Some description'
+    },
+    {
+      src: "https://i.imgur.com/3S1OoAi.jpg",
+      title: "Some title",
+      description: 'Some description'
+    },
+    {
+      src: "https://i.imgur.com/3GHYEo3.jpg",
+      title: "Some title",
+      description: 'Some description'
+    },
+    {
+      src: "https://i.imgur.com/ywAiaWh.jpg",
+      title: "Some title",
+      description: 'Some description'
+    },
+    {
+      src: "https://i.imgur.com/wOPcRBD.jpg",
+      title: "Some title",
+      description: 'Some description'
+    },
+    {
+      src: "https://i.imgur.com/Z9dsNRc.jpg",
+      title: "Some title",
+      description: 'Some description'
+    },
+    {
+      src: "https://i.imgur.com/3HXgWpU.jpg",
+      title: "Some title",
+      description: 'Some description'
+    }
   ]
 
-  constructor() { }
+  constructor(private modalService: BsModalService) {}
 
   ngOnInit(): void {
-    this.selected = 'All'
+    this.selectedCategory = 'All'
   }
 
   setActive(gallery: string) {
-    this.selected = gallery
+    this.selectedCategory = gallery
+  }
+
+  onClick(index: number, template: TemplateRef<any>) {
+      this.selectedImage = this.images[index]
+      this.modelRef = this.modalService.show(template)
   }
 }
