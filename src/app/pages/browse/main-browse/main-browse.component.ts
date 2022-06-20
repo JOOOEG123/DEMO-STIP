5b1d0fae19f83ecc45daec73fe14f2bdd160b679
Yule Zhang <71297348+yzhang54@users.noreply.github.com>
2022-06-19 23:11:22 -0400

    Browse page design
    Add more design code for browse page.

--- src/app/pages/browse/main-browse/main-browse.component.ts
+++ src/app/pages/browse/main-browse/main-browse.component.ts
@@ -5,7 +5,6 @@ import { NgxMasonryOptions } from 'ngx-masonry';
 
 import { UpdateRowsPipe } from 'src/app/pipe/update-rows-pipe.pipe'
 
-import { adminRoutes, pagesRoutes } from '../../../pages';
 
 @Component({
   selector: 'app-main-browse',
