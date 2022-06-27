3914ae1b19474b9f5956dbdffefd87bf86b176b0
weilonp <104396688+weilonp@users.noreply.github.com>
2022-06-27 16:04:41 -0400

    Upload tab
    Responsive

--- src/app/pages/account/upload/upload.component.ts
+++ src/app/pages/account/upload/upload.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
@@ -8,6 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class UploadComponent implements OnInit {
+
+  selected?: string;
+  ethnicGroup: string[] = [
+    'option1',
+    'option2'
+  ]
+
+  selected2?: string;
+  occupation: string[] = [
+    'teacher',
+    'student'
+  ]
 
   constructor() { }
 
