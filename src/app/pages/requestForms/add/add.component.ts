2e12371f4170cfadf128c40d3ede99cad845c396
weilonp <104396688+weilonp@users.noreply.github.com>
2022-06-28 14:49:37 -0400

    Add page
    Almost Same as upload page, just need some editing

--- src/app/pages/requestForms/add/add.component.ts
+++ src/app/pages/requestForms/add/add.component.ts
@@ -1,15 +1,116 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
   styleUrls: ['./add.component.scss']
+  
 })
+
 export class AddComponent implements OnInit {
+
+  selected?: string;
+  ethnicGroup: string[] = [
+    'Zhuang',
+    'Hui',
+    'Manchu',
+    'Uyghur',
+    'Miao',
+    'Yi',
+    'Tujia',
+    'Tibetan',
+    'Mongol',
+    'Dong',
+    'Bouyei',
+    'Yao',
+    'Bai',
+    'Joseonjok',
+    'Hani',
+    'Li',
+    'Kazakh',
+    'Dai',
+    'She',
+    'Lisu',
+    'Dongxiang',
+    'Gelao',
+    'Lahu',
+    'Wa',
+    'Sui',
+    'Nakhi',
+    'Qiang',
+    'Tu',
+    'Mulao',
+    'Xibe',
+    'Kyrgyz',
+    'Jingpo',
+    'Daur',
+    'Salar',
+    'Blang',
+    'Maonan',
+    'Tajik',
+    'Pumi',
+    'Achang',
+    'Nu',
+    'Evenki',
+    'Vietnamese',
+    'Jino',
+    'De ang',
+    'Bonan',
+    'Russian',
+    'Yugur',
+    'Uzbek',
+    'Monba',
+    'Oroqen',
+    'Derung',
+    'Hezhen',
+    'Gaoshan',
+    'Lhoba',
+    'Tatars',
+    'Undistinguished',
+    'Naturalized Citizen'
+  ]
+
+  selected2?: string;
+  occupation: string[] = [
+    'Deputy',
+    'Director',
+    'Professor',
+    'Worker',
+    'Actress',
+    'Agent',
+    'Assistant',
+    'Editor-in-Chief',
+    'Assistant Lieutenant',
+    'Assistant Professor',
+    'Associate Professor',
+    'Cadre',
+    'Chairman',
+    'Chief',
+    'Chief Engineer',
+    'Christian Priest',
+    'Clerk',
+    'Deputy Director',
+    'Deputy Secretary',
+    'Deputy Secretary General'
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  url=""
+
+  onselectFile(e){
+    if(e.target.files){
+      var reader = new FileReader();
+      reader.readAsDataURL(e.target.files[0]);
+      reader.onload=(event:any)=>{
+        this.url=event.target.result;
+      }
+    }
+  }
+
 }
+
