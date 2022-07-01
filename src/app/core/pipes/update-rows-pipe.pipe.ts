84a380a68f791b6efe79a50023c1008384575ad1
Yule Zhang <71297348+yzhang54@users.noreply.github.com>
2022-07-01 12:43:16 -0400

    Browse page
    Add IDs for each search result.

--- src/app/core/pipes/update-rows-pipe.pipe.ts
+++ src/app/core/pipes/update-rows-pipe.pipe.ts
@@ -6,11 +6,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class UpdateRowsPipe implements PipeTransform {
 
+  updateRes: any[] = []
+
   transform<T>(value: T[], perRow: number): T[][] {
     let updated_db_result: T[][] = [];
     for (let i = 0; i < value.length; i += perRow) {
       updated_db_result.push(value.slice(i, i + perRow))
     }
+    this.updateRes = updated_db_result
     return updated_db_result;
   }
 
