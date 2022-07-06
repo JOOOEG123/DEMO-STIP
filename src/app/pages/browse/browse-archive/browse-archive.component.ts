d4625a628709aafcc6f7452a8b9629054e64777a
Yule Zhang <71297348+yzhang54@users.noreply.github.com>
2022-07-05 20:29:43 -0400

    Add
    Enable the "Download PDF file" button for the memoir page.

--- src/app/pages/browse/browse-archive/browse-archive.component.ts
+++ src/app/pages/browse/browse-archive/browse-archive.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 import { ArchieveApiService } from 'src/app/core/services/archives-api-service';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
-
+import { jsPDF } from 'jspdf';
 
 @Component({
   selector: 'app-browse-archive',
@@ -11,17 +11,22 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
   styleUrls: ['./browse-archive.component.scss'],
 })
 export class BrowseArchiveComponent implements OnInit {
+  @ViewChild('content') content!: ElementRef;
 
+  [x: string]: any;
   // solution 1
   id = this.route.snapshot.paramMap.get('id') as string;
-  profile =  {} as any;
+  profile = {} as any;
 
-  
-  constructor(  private route: ActivatedRoute,private router: Router,private arch: ArchieveApiService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private arch: ArchieveApiService
+  ) {}
 
   ngOnInit(): void {
     console.log(this.id);
-    this.arch.getPersonById(this.id).subscribe(res => {
+    this.arch.getPersonById(this.id).subscribe((res) => {
       console.log(res);
       this.profile = res;
     });
@@ -29,5 +34,22 @@ export class BrowseArchiveComponent implements OnInit {
     // this.route.paramMap.subscribe((params: ParamMap) => {
     //   console.log(params.get('id'));
     // });
+  }
+
+  public SavePDF(): void {
+    let content = this.content.nativeElement;
+    console.log(content);
+    let doc = new jsPDF('p', 'pt', 'a4');
+    let _elementHandlers = {
+      '#editor': function (element: any, renderer: any) {
+        return true;
+      },
+    };
+    doc.html(content, {
+      callback: function (doc) {
+        doc.save('profile.pdf');
+      },
+    });
+    //doc.output('dataurlnewwindow'); // just open it
   }
 }
