ef501259f107f6e7d080bcdc0f029e135a29828e
dangan114 <an.dang114@gmail.com>
2022-07-12 00:34:24 -0400

    Update contribution and upload page

--- src/app/pages/admin/approval/approval.component.ts
+++ src/app/pages/admin/approval/approval.component.ts
@@ -75,12 +75,6 @@ export class ApprovalComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void { 
-    //  console.log(this.archApi.addContribution(result))
-    // this.archApi.getContributions().snapshotChanges((data: any) => {
-    //   console.log(data)
-    // })
-
-  
     this.subcription = this.contributionAPI.fetchAllContribution().subscribe((data: any) => {
       
       this.contributions.length = 0
@@ -119,10 +113,6 @@ export class ApprovalComponent implements OnInit, OnDestroy {
           this.rejectedContributions.push(data)
         }
       }
-      // this.selectedContributions =  JSON.parse(JSON.stringify(this.newContributions));
-
-      // console.log(this.selectedContributions, this.newContributions, this.approvedContributions, this.rejectedContributions)
-      
     })
   }
 
@@ -131,67 +121,38 @@ export class ApprovalComponent implements OnInit, OnDestroy {
   }
   
 
-  onApprove(contributionId: string) {
+  onApprove(contribution: Contribution) {
+    this.modalRef?.hide()
     this.publish = 'approved'
     const index = this.selectedContributions.findIndex(
-      (contribution) => contribution.contributionId == contributionId
+      (c) => c.contributionId == contribution.contributionId
     );
     this.selectedContribution = this.newContributions[index];
     this.selectedContribution.state = 'removed';
-    // this.approvedContributions.unshift(this.selectedContribution)
   }
 
-  // onEdit(template: TemplateRef<any>, rightistId: string) {
-  //   this.modalRef = this.modalService.show(template);
-  // }
-
-  // onPending(victimId: string) {
-  //   const index = this.selectedContributions.findIndex(
-  //     (contribution) => contribution.contributionId == victimId
-  //   )
-  //   this.selectedContribution = this.newContributions[index];
-  //   this.selectedContribution.state = 'removed';
-  //   this.pendingContributions.unshift(this.selectedContribution);
-  // }
-
-  onReject(contributionId: string) {
+  onReject(contribution: Contribution) {
+    this.modalRef?.hide()
     this.publish = 'rejected'
-
     const index = this.selectedContributions.findIndex(
-      (contribution) => contribution.contributionId == contributionId
+      (c) => c.contributionId == contribution.contributionId
     );
-    // if (index == -1) {
-    //   const i = this.pendingContributions.findIndex(
-    //     (contribution) => contribution.contributionId == contributionId
-    //   )
-    //   this.selectedContribution = this.pendingContributions[i]
-    //   this.selectedContribution.state = 'removed'
-    //   this.rejectedContributions.unshift(this.selectedContribution)
-    // }
    
     this.selectedContribution = this.newContributions[index];
     this.selectedContribution.state = 'removed';
-    // this.rejectedContributions.unshift(this.selectedContribution); 
-    // this.archApi.updateContributionPublishAttribute(this.selectedContribution.contributionId, publish)
   }
 
-  onReconsider(contributionId: string) {
+  onReconsider(contribution: Contribution) {
+    this.modalRef?.hide()
+    this.publish = 'approved'
     const index = this.selectedContributions.findIndex(
-      (contribution) => contribution.contributionId = contributionId
+      (c) => c.contributionId = contribution.contributionId
     )
     this.selectedContribution = this.rejectedContributions[index]
     this.selectedContribution.state = 'removed'
-    // this.newContributions.unshift(this.selectedContribution)
   }
 
   setActiveCategory(category: Categories) {
-    // const index = this.selectedContributions.findIndex(
-    //   (contribution) => contribution.contributionId == this.selectedContribution?.contributionId
-    // )
-
-    // if (index != -1) {
-    //   this.selectedContributions[index].limit = 3
-    // }
     this.activeCategory = category;
     this.selectedContributions = this.categories[this.activeCategory];
     
@@ -203,33 +164,6 @@ export class ApprovalComponent implements OnInit, OnDestroy {
   }
 
   animationDone(event: AnimationEvent) {
-    // if (this.activeCategory == 'Rejected Contributions') {
-    //   if (event.toState == 'removed') {
-    //     // const index = this.rejectedContributions.findIndex(
-    //     //   (contribution) => contribution.contributionId == this.selectedContribution.contributionId
-    //     // )
-    //     // this.rejectedContributions.splice(index, 1)
-    //     this.selectedContribution.state = 'void'
-    //   }
-    // }
-
-    // if (this.activeCategory == 'Approved Contributions') {
-    //   if (event.toState == 'removed') {
-    //     // this.approvedContributions.unshift(this.selectedContribution); 
-    //     this.selectedContribution.state = 'void'
-    //   }
-    // }
-
-    // if (this.activeCategory == 'New Contributions') {
-    //   if (event.toState == 'removed') {
-    //     // const index = this.newContributions.findIndex(
-    //     //   (contribution) => contribution.contributionId == this.selectedContribution?.contributionId
-    //     // );
-    //     // this.newContributions.splice(index, 1);
-    //     this.selectedContribution.state = 'void';
-    //   }
-    // }
-
     this.disabled = false
     if (this.selectedContribution) {
       let contributorId = 
@@ -245,12 +179,6 @@ export class ApprovalComponent implements OnInit, OnDestroy {
 
   onReadMore(template: TemplateRef<any>, contribution: Contribution) {
     this.selectedContribution = contribution
-    this.modalRef = this.modalService.show(template)
-    // const index = this.selectedContributions.findIndex(
-    //   (contribution) => contribution.contributionId == contributionId
-    // )
-    // this.selectedContribution = this.selectedContributions[index]  
-    // this.selectedContributions[index].limit = this.selectedContributions[index].rightist['events'].length
-    // }
+    this.modalRef = this.modalService.show(template, { class: 'modal-lg'})
   }
 }
--- src/app/pages/requestForms/upload/upload.component.ts
+++ src/app/pages/requestForms/upload/upload.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import {
   FormControl,
   FormGroup,
@@ -9,6 +9,7 @@ import {
 import { Router, RouterLink } from '@angular/router';
 import { AuthServiceService } from 'src/app/core/services/auth-service.service';
 import { ContributionsService } from 'src/app/core/services/contributions.service';
+import { Contribution, Rightist } from 'src/app/core/types/adminpage.types';
 import { UUID } from 'src/app/core/utils/uuid';
 
 @Component({
@@ -17,8 +18,32 @@ import { UUID } from 'src/app/core/utils/uuid';
   styleUrls: ['./upload.component.scss'],
 })
 export class UploadComponent implements OnInit {
+  @Input() contribution?: Contribution;
+  @Input() page?: string
+  @Output() approve: EventEmitter<Contribution> = new EventEmitter()
+  @Output() reject: EventEmitter<Contribution> = new EventEmitter()
+  @Output() reconsider: EventEmitter<Contribution> = new EventEmitter()
   //image url
   url = '';
+
+  onApprove() {
+    if (this.contribution) {
+      this.approve.emit({...this.contribution})
+    }
+  }
+
+  onReject() {
+    if (this.contribution) {
+      this.reject.emit({...this.contribution})
+    }
+  }
+  
+  onReconsider() {
+    if (this.contribution) {
+      this.reconsider.emit({...this.contribution})
+    }
+  }
+
 
   form = new FormGroup({
     name: new FormControl('', Validators.required),
@@ -38,7 +63,7 @@ export class UploadComponent implements OnInit {
 
   eventArray = new FormArray([this.newEvent()]);
 
-  memiorArray = new FormArray([this.newMemoir()]);
+  memoirArray = new FormArray([this.newMemoir()]);
 
   imageForm = new FormGroup({
     imageUpload: new FormControl(''),
@@ -52,6 +77,7 @@ export class UploadComponent implements OnInit {
       event: new FormControl(''),
     });
   }
+
   private newMemoir() {
     return new FormGroup({
       memoirTitle: new FormControl(''),
@@ -65,7 +91,7 @@ export class UploadComponent implements OnInit {
   }
 
   get memiorControls() {
-    return this.memiorArray.controls as FormGroup[];
+    return this.memoirArray.controls as FormGroup[];
   }
 
   clear() {
@@ -77,7 +103,7 @@ export class UploadComponent implements OnInit {
     this.form2.reset();
     this.imageForm.reset();
     this.eventArray.reset();
-    this.memiorArray.reset();
+    this.memoirArray.reset();
   }
 
   clearImage() {
@@ -178,11 +204,11 @@ export class UploadComponent implements OnInit {
   }
 
   addMemoir() {
-    this.memiorArray.push(this.newMemoir());
+    this.memoirArray.push(this.newMemoir());
   }
 
   removeMemoir(i: number) {
-    this.memiorArray.removeAt(i);
+    this.memoirArray.removeAt(i);
   }
 
   constructor(
@@ -191,7 +217,60 @@ export class UploadComponent implements OnInit {
     private route: Router
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log(this.contribution)
+
+    if (this.contribution?.contributionId) {
+      const rightist: Rightist = this.contribution.rightist
+      this.form = new FormGroup({
+        name: new FormControl(rightist.lastName + ' ' + rightist.firstName, Validators.required),
+        gender: new FormControl(rightist.gender),
+        status: new FormControl(rightist.status),
+        ethnic: new FormControl(rightist.ethnicity),
+        occupation: new FormControl(rightist.job, Validators.required),
+        rightestYear: new FormControl(rightist.rightistYear, Validators.required),
+        birthYear: new FormControl(rightist.birthYear, Validators.required),
+      });
+    
+      this.form2 = new FormGroup({
+        imageUpload: new FormControl(''),
+        image: new FormControl(''),
+        content: new FormControl('')
+      });
+
+      for (const event of this.contribution.rightist.events) {
+        this.eventArray.push(new FormGroup({
+          startYear: new FormControl(event.startYear),
+          endYear: new FormControl(event.endYear),
+          event: new FormControl(event.event),
+        }))
+      }
+
+      for (const memoir of this.contribution.rightist.memoirs) {
+        this.memoirArray.push(new FormGroup({
+          memoirTitle: new FormControl(memoir.memoirTitle),
+          memoirContent: new FormControl(memoir.memoirContent),
+          memoirAuthor: new FormControl(memoir.memoirAuthor),
+        }))
+      }
+    }
+  }
+
+  // onApprove() {
+  //   if (this.contribution) {
+  //     let contributorId = this.contribution.contributorId[this.contribution.contributorId.length - 1]
+  //     this.contributionService.updateContributionByPublish(
+  //       contributorId , this.contribution.contributionId, 'approved')
+  //   }
+  // }
+
+  // onReject() {
+  //   if (this.contribution) {
+  //     let contributorId = this.contribution.contributorId[this.contribution.contributorId.length - 1]
+  //     this.contributionService.updateContributionByPublish(
+  //       contributorId , this.contribution.contributionId, 'rejected')
+  //   }
+  // }
 
   onselectFile(e) {
     if (e.target.files) {
@@ -231,7 +310,7 @@ export class UploadComponent implements OnInit {
         detailJob: '',
         workplace: '',
         events: this.eventArray.value,
-        memoirs: this.memiorArray.value,
+        memoirs: this.memoirArray.value,
         reference: '',
         description: content,
       },
