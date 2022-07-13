cfee905dc58a88ebcee04293950d39db7dd7f598
weilonp <104396688+weilonp@users.noreply.github.com>
2022-07-13 09:36:38 -0400

    Added the request to modify page and button

---
+++ src/app/pages/requestForms/upload/modifyRequest/modify-request/modify-request.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ModifyRequestComponent } from './modify-request.component';
+
+describe('ModifyRequestComponent', () => {
+  let component: ModifyRequestComponent;
+  let fixture: ComponentFixture<ModifyRequestComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ModifyRequestComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ModifyRequestComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/requestForms/upload/modifyRequest/modify-request/modify-request.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modify-request',
+  templateUrl: './modify-request.component.html',
+  styleUrls: ['./modify-request.component.scss']
+})
+export class ModifyRequestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
