37874f4c6bd96447a9176ff4fbb55b0804b79131
Yule Zhang <71297348+yzhang54@users.noreply.github.com>
2022-07-14 13:24:11 -0400

    Memoir page and browse page.
    1. add "dropdown" for the search bar and relative searching functions.
    2. add design style for memoir page.
    3. code clean up

--- functions/lib/config/firebase.js
+++ functions/lib/config/firebase.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.admin = exports.storage = exports.database = exports.fireStore = void 0;
 const admin = require("firebase-admin");
 exports.admin = admin;
-//import * as functions from 'firebase-functions';
+// import * as functions from 'firebase-functions';
 admin.initializeApp({
     credential: admin.credential.applicationDefault(),
     databaseURL: 'https://stip-demo-default-rtdb.firebaseio.com',
