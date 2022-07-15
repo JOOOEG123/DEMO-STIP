11ab9c8618a86ae16565d3d05cd5616fc55906ed
Yule Zhang <71297348+yzhang54@users.noreply.github.com>
2022-07-15 13:48:32 -0400

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,7 +20,7 @@ const routes: Routes = [
         path: 'home',
         component: HomepageComponent,
         data: {
-          title: 'hinese “Rightist” Archives',
+          title: 'Chinese “Rightist” Archives',
         },
       },
       ...pagesRoutes,
