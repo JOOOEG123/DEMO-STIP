48e41b86eb6599477d02bd397602d5eff9487150
weilonp <104396688+weilonp@users.noreply.github.com>
2022-07-19 11:27:47 -0400

    Profile now constants
    Further CSS needed for overlay text align

--- src/app/pages/about/AboutTeam/about-team.component.ts
+++ src/app/pages/about/AboutTeam/about-team.component.ts
@@ -1,61 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  TEAM_PROFILES,
+  Profile,
+} from 'src/app/pages/about/AboutTeam/about-team.constants';
 
 @Component({
   selector: 'app-about-team',
   templateUrl: './about-team.component.html',
-  styleUrls: ['./about-team.component.scss']
+  styleUrls: ['./about-team.component.scss'],
 })
 export class AboutTeamComponent implements OnInit {
+  team_profile: Profile[] = TEAM_PROFILES;
 
-  constructor() { }
-  Profiles = [
-    {
-      name: 'Joel Adeniji',
-      email: 'johnDoe@aol.com',
-      profile: 'default-profile.png'
-    },
-    {
-      name: 'An Dang',
-      email: 'JaneDoe@aol.com',
-      profile: 'default-profile.png'
-    },
-    {
-      name: 'Bailey George',
-      email: 'johnSmith@aol.com',
-      profile: 'default-profile.png'
-    },
-    {
-      name: 'Kristie',
-      email: 'johnSmith@aol.com',
-      profile: 'default-profile.png'
-    },
-    {
-      name: 'Weilon Price',
-      email: 'johnSmith@aol.com',
-      profile: 'default-profile.png'
-    },
-    {
-      name: 'Yule Zhang',
-      email: 'johnSmith@aol.com',
-      profile: 'default-profile.png'
-    },
-    {
-      name: 'Shanshan Cui',
-      email: 'johnSmith@aol.com',
-      profile: 'default-profile.png'
-    },
-    {
-      name: 'Myeong Lee',
-      email: 'johnSmith@aol.com',
-      profile: 'default-profile.png'
-    },
-    {
-      name: 'Zhehan Zhang',
-      email: 'johnSmith@aol.com',
-      profile: 'default-profile.png'
-    },
-  ];
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
