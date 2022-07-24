7000ff6d7d3428d3c211b83b27a5968e1c28f99a
Joel Adeniji <57308580+JOOOEG123@users.noreply.github.com>
2022-07-24 19:19:10 -0400

    contact us

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -2,10 +2,12 @@ import { ModalTemplateComponent } from './modal-template/modal-template.componen
 import { StipSpinnerComponent } from './stip-spinner/stip-spinner.component';
 import { ViewComponent } from './View/view.component';
 import { ChartComponent } from '../pages/about/chart/chart.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 export const sharedComponents = [
-    ModalTemplateComponent, 
-    StipSpinnerComponent, 
-    ViewComponent,
-    ChartComponent
+  ModalTemplateComponent,
+  StipSpinnerComponent,
+  ViewComponent,
+  ChartComponent,
+  ContactUsComponent,
 ];
