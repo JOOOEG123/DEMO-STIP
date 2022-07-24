25a0f39390749e3142445643b179b771eb216743
Yule Zhang <71297348+yzhang54@users.noreply.github.com>
2022-07-24 00:28:24 -0400

    Add css design and fix bugs

--- src/app/pages/browse/repository/repository.component.ts
+++ src/app/pages/browse/repository/repository.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 import { NgxMasonryOptions } from 'ngx-masonry';
 
 @Component({
@@ -7,13 +8,10 @@ import { NgxMasonryOptions } from 'ngx-masonry';
   styleUrls: ['./repository.component.scss'],
 })
 export class RepositoryComponent implements OnInit {
+  chinese_repository_titles: any[][] = [];
+  pdf = { Foward: ['A large dictionary of the names of the victims in 1957'] };
   repository_titles: any[][] = [
-    [
-      'Forward',
-      'Forward Title',
-      'Document',
-      ['A large dictionary of the names of the victims in 1957'],
-    ],
+    ['Forward', 'Forward Title', 'Document'],
     ['Publication Notes', ''],
     [
       'Appendix',
@@ -42,6 +40,28 @@ export class RepositoryComponent implements OnInit {
         'Mao Zedong things are changing Central Political Research Office',
         'Qian_liqun_ΘÆ▒τÉåτ╛ñ',
       ],
+      [
+        '0中共中央划分右派分子的标准',
+        '0毛泽东事情正在起变化 中央政治研究室',
+        '1罗瑞卿报告',
+        '2云南省地县整风运动和反右派斗争综述',
+        '3晓明广西省',
+        '4杨金国河南省淇县',
+        '5荥经民盟简史',
+        '6宁陕县1957年整风运动和反右派斗争综述',
+        '7林圣智浙江省平阳县',
+        '8李新（中国人民大学）反右亲历记',
+        '9施绍箕（上海交通大学）',
+        '10吴中杰复旦往事',
+        '11江西省信丰县人民检察院关于金盆山垦殖场右派分子情况调查报告',
+        '12反右运动到底要整什么人',
+        '13劳动教养和反右派斗争',
+        '14中国五大劳改营掠影',
+        '15陈宗培揭开当代秦始皇的面纱',
+        '16陆平关于处理右派分子的报告',
+        '17右派大家  这是一个毁灭文化',
+        '18河南农场监狱的黒幕',
+      ],
     ],
     [
       'Preface',
@@ -64,23 +84,52 @@ export class RepositoryComponent implements OnInit {
         'Quotes from Deng Xiaoping',
         'Text by Mr. Yan Changgui',
       ],
+      [
+        '万学仁先生序',
+        '冯士彦先生信',
+        '张清扬先生信',
+        '朱健国先生序',
+        '杜光先生序',
+        '杨教先生序',
+        '杨锡九先生信',
+        '王学俭先生信',
+        '茅于轼先生题词',
+        '裴毅然先生序',
+        '赵旭先生序',
+        '邓小平语录',
+        '金钟先生序',
+        '阎长贵先生赐文',
+        '魏燮中先生序',
+      ],
     ],
   ];
 
   selectedCategory: string = this.repository_titles[0][0];
-  constructor() {}
 
-  downloadCont = [
-    {
-      name: 'Forward files',
-      filename: 'A large dictionary of the names of the victims in 1957',
-    },
-  ];
+  currentLanguage = this.translate.currentLang;
+  constructor(private translate: TranslateService) {
+    console.log(this.currentLanguage);
+    console.log(this.selectedCategory);
+  }
 
   ngOnInit(): void {}
 
   setActive(gallery: string) {
     this.selectedCategory = gallery;
+  }
+
+  switchLanguage() {
+    switch (this.currentLanguage) {
+      case 'cn': {
+        this.pdf = { Foward: ['一九五七年受难者姓名大辞典'] };
+        break;
+      }
+      default: {
+        this.pdf = {
+          Foward: ['A large dictionary of the names of the victims in 1957'],
+        };
+      }
+    }
   }
 
   downloadFromAsset(filename: string, folder: string) {
