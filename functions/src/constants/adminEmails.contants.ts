f73cd7c7f5219774e4e3bd4825f391a34ef93da5
weilonp <104396688+weilonp@users.noreply.github.com>
2022-07-25 11:37:43 -0400

    Update adminEmails.contants.ts
    Added developers emails

--- functions/src/constants/adminEmails.contants.ts
+++ functions/src/constants/adminEmails.contants.ts
@@ -5,4 +5,7 @@ export const ALL_ADMIN_EMAIL = [
   'deeper.myeong.lee@gmail.com',
   'scui1@gmu.edu',
   'jadeniji@gmu.edu',
+  'wprice6@gmu.edu',
+  'yulez995@gmail.com',
+  'an.dang114@gmail.com',
 ];
