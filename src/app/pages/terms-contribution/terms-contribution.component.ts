5e2c956049883ffe59cdd254e359501895d557e3
Joel Adeniji <57308580+JOOOEG123@users.noreply.github.com>
2022-08-03 14:43:36 -0400

    firebase.

---
+++ src/app/pages/terms-contribution/terms-contribution.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-terms-contribution',
+  templateUrl: './terms-contribution.component.html',
+  styleUrls: ['./terms-contribution.component.scss']
+})
+export class TermsContributionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
