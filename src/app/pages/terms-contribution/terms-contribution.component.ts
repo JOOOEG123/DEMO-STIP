57026b5345eb245a4e65bdea360c1f109471c6b6
Joel Adeniji <57308580+JOOOEG123@users.noreply.github.com>
2022-08-04 04:28:02 -0400

    complete

--- src/app/pages/terms-contribution/terms-contribution.component.ts
+++ src/app/pages/terms-contribution/terms-contribution.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+type Views = 'T' | 'P' | 'F';
+
+const HeadTitles = {
+  T: 'Terms of Contribution',
+  P: 'Privacy Policy',
+  F: 'Frequently Asked Questions',
+};
+
 @Component({
   selector: 'app-terms-contribution',
   templateUrl: './terms-contribution.component.html',
-  styleUrls: ['./terms-contribution.component.scss']
+  styleUrls: ['./terms-contribution.component.scss'],
 })
 export class TermsContributionComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  curView: Views = 'T';
+  header_titles = HeadTitles;
+  get curHeaderTitle() {
+    return this.header_titles[this.curView];
   }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
